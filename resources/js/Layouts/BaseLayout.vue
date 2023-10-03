3fccabcad41d35b7ed1d46ae755659e52a53a780
Andrii Prykhodko <andriichello@gmail.com>
2023-10-03 16:44:17 +0300

    Set up Vue.js and Inertia.js

---
+++ resources/js/Layouts/BaseLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <main class="min-w-screen min-h-screen flex flex-col justify-center items-center">
+    <slot></slot>
+  </main>
+</template>
+
+<script setup>
+</script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <BaseLayout>
+    <div class="flex flex-col justify-center items-center gap-3">
+      <h3 class="text-3xl font-bold underline">Home page</h3>
+      <p class="text-xl font-light px-3">
+        This is a home page of the Checkers app, written using
+        Laravel and Vue.js with Inertia.js
+      </p>
+    </div>
+  </BaseLayout>
+</template>
+
+<script setup>
+import BaseLayout from "../Layouts/BaseLayout.vue";
+</script>
