79222cba54591928572b62834365f4fddf4c7d0c
Andrii Prykhodko <andriichello@gmail.com>
2023-10-08 10:15:02 +0300

    Create RedirectIfUnauthenticated middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,8 +39,11 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        return array_merge(parent::share($request), [
-            //
-        ]);
+        return array_merge(
+            parent::share($request),
+            [
+                'user' => $request->user(),
+            ]
+        );
     }
 }
---
+++ app/Http/Middleware/RedirectIfUnauthenticated.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class RedirectIfAuthenticated.
+ */
+class RedirectIfUnauthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next, string ...$guards): Response
+    {
+        if (!$request->user()) {
+            return Redirect::route('login.view');
+        }
+
+        return $next($request);
+    }
+}
